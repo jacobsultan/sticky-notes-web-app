ff4177c41ac2402d768eab239ed5980eadc81e14
Jacob Sultan <jsultanabc@gmail.com>
2024-02-29 15:54:54 +0000

    fixed error 'templates' over 'template'

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+# initialised flask and the root path
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-@app.route('/')
-def home():
-    return 'Welcome to the Home Page!'
+notes = []
+
+@app.route('/', methods=['GET', 'POST'])
+# root accepts these methods to get info from server and post data
+
+def index():
+    if request.method == "POST": #if the forms been submitted
+        note = request.form["note"] #retrieves note
+        notes.append(note) #stores it 
+        return redirect(url_for('index')) #returns client to index page
+    else:
+        return render_template('index.html', notes=notes) #if its a get request opens index.html
+
+if __name__ == '__main__':
+    app.run(debug=True)
