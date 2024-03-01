115314a67cf47a0b9c184f5776160ec387cb10e1
Jacob Sultan <jsultanabc@gmail.com>
2024-03-01 16:38:07 +0000

    saving notes in db, can edit but cant save edits

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -23,12 +23,16 @@ def index():
         note_content = request.form["note"] #retrieves note
         if note_content: #making sure the note isn't empty
             new_note = Note(content=note_content) #creates a note instance for the model
-            db.session.add(new_note) #like staging for git
-            db.session.commit() #like commiting for git
+            print(new_note)
+            try:
+                db.session.add(new_note)
+                db.session.commit()
+            except Exception as e:
+                print("Failed to add note:", e)
+                db.session.rollback()  # Rollback the session in case of error
         return redirect(url_for('index')) #preventative measure for resubmitting 
-    else:
-        notes = Note.query.all() #sqlachemy to request all notes to show
-        return render_template('index.html', notes=notes) #this renders all the notes found in the db though index
+    notes = Note.query.all() #sqlachemy to request all notes to show
+    return render_template('index.html', notes=notes) #this renders all the notes found in the db though index
 
 
 @app.route('/delete/<int:note_id>', methods=['POST']) #new route for deleting notes, passes the note_id an int into the delete_note function
@@ -39,10 +43,22 @@ def delete_note(note_id):
         db.session.commit() #acc passes through (likely issue found here if present)
     return redirect(url_for('index')) # takes back to index
 
-
-
-
+@app.route('/edit/<int:note_id>', methods=['POST'])
+def edit_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    edited_content = request.form['content'].strip()
+    if edited_content:
+        note.content = edited_content
+        db.session.commit()
+        flash('Note updated successfully.', 'success')
+    else:
+        flash('Note content cannot be blank.', 'error')
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+import os
+print("Current working directory:", os.getcwd())
